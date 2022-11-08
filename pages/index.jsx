02aaa15bc5acafc7401a23770992ec87ad628a2b
AlexandraM07 <110460669+AlexandraM07@users.noreply.github.com>
2022-11-08 18:59:18 +0200

    MC

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>Hello Monochrome <i className="fa-solid fa-house"></i></div>
+    <div>
+      <h1>Hello Monochrome <i className="fa-solid fa-house"></i></h1>
+      <button className="button button-primary button-large">view all</button>
+      <button className="button button-inverted">view all</button>
+      <button className="button button-small">view all</button>
+
+      <img src="/images/banner.png" alt="Alt" />
+    </div>
+
   )
 }
+
+
