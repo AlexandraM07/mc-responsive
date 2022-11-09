c26e7e7f7f4cfc57f44e64222a84a73ffae22b0d
AlexandraM07 <110460669+AlexandraM07@users.noreply.github.com>
2022-11-09 19:17:57 +0200

    MC

---
+++ pages/index-setup.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        Hello Monochrome <i className="fa-solid fa-house"></i>
+      </h1>
+      <button className="button button-primary">view all</button>
+      <button className="button button-secondary">submit</button>
+
+      <img src="/images/banner.png" alt="Alt" />
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
-      <h1>Hello Monochrome <i className="fa-solid fa-house"></i></h1>
-      <button className="button button-primary button-large">view all</button>
-      <button className="button button-inverted">view all</button>
-      <button className="button button-small">view all</button>
-
-      <img src="/images/banner.png" alt="Alt" />
+      <header className="header">
+        <Header></Header>
+      </header>
     </div>
-
-  )
+  );
 }
-
-
